9c21a4291ea3390158e9f5c25ddf5e033bd3d8f8
irisktaker <ponanzar@gmail.com>
2022-01-21 12:48:24 +0200

    flutter_tutorial

--- lib/section5_bmi_app/bmi_app.dart
+++ lib/section5_bmi_app/bmi_app.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_string_interpolations
+
 import 'package:flutter/material.dart';
 
 class BMIApp extends StatefulWidget {
@@ -7,6 +9,8 @@ class BMIApp extends StatefulWidget {
   _BMIAppState createState() => _BMIAppState();
 }
 
+double sliderValue = 10;
+
 class _BMIAppState extends State<BMIApp> {
   @override
   Widget build(BuildContext context) {
@@ -14,6 +18,116 @@ class _BMIAppState extends State<BMIApp> {
       appBar: AppBar(
         title: const Text("Body Mass Index"),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: DefaultTextStyle(
+          style: const TextStyle(
+              fontSize: 35, fontWeight: FontWeight.bold, color: Colors.black),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    buildCard(
+                      title: "Male",
+                      icon: Icons.male,
+                    ),
+                    const SizedBox(width: 20),
+                    buildCard(
+                      title: "Female",
+                      icon: Icons.female,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.teal.shade300,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text("Height"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            "${sliderValue.toStringAsFixed(1)}",
+                            style: const TextStyle(
+                                fontSize: 40, color: Colors.white),
+                          ),
+                          const SizedBox(width: 6),
+                          const Text(
+                            "CM",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Slider(
+                        value: sliderValue,
+                        max: 220,
+                        min: 50,
+                        onChanged: (value) {
+                          setState(() {});
+
+                          sliderValue = value;
+                          print(value);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [buildCard(title: 'test', icon: Icons.ac_unit)],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(flex: 1, child: Container())
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded buildCard({
+    required String? title,
+    required IconData? icon,
+    void Function()? onTap,
+  }) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.teal.shade300,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                icon,
+                color: Colors.white,
+                size: 120,
+              ),
+              Text(title!)
+            ],
+          ),
+        ),
       ),
     );
   }
