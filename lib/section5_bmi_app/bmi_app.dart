b2b1a84096b3949a02237a4f4b342a80dd516364
irisktaker <ponanzar@gmail.com>
2022-01-21 13:19:49 +0200

    flutter_tutorial

--- lib/section5_bmi_app/bmi_app.dart
+++ lib/section5_bmi_app/bmi_app.dart
@@ -9,7 +9,9 @@ class BMIApp extends StatefulWidget {
   _BMIAppState createState() => _BMIAppState();
 }
 
-double sliderValue = 10;
+double sliderValue = 120;
+double weight = 60;
+int age = 18;
 
 class _BMIAppState extends State<BMIApp> {
   @override
@@ -19,84 +21,198 @@ class _BMIAppState extends State<BMIApp> {
         title: const Text("Body Mass Index"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: DefaultTextStyle(
-          style: const TextStyle(
-              fontSize: 35, fontWeight: FontWeight.bold, color: Colors.black),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    buildCard(
-                      title: "Male",
-                      icon: Icons.male,
-                    ),
-                    const SizedBox(width: 20),
-                    buildCard(
-                      title: "Female",
-                      icon: Icons.female,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.teal.shade300,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: DefaultTextStyle(
+            style: const TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      const Text("Height"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Text(
-                            "${sliderValue.toStringAsFixed(1)}",
-                            style: const TextStyle(
-                                fontSize: 40, color: Colors.white),
-                          ),
-                          const SizedBox(width: 6),
-                          const Text(
-                            "CM",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
+                      buildCard(
+                        title: "Male",
+                        icon: Icons.male,
                       ),
-                      Slider(
-                        value: sliderValue,
-                        max: 220,
-                        min: 50,
-                        onChanged: (value) {
-                          setState(() {});
-
-                          sliderValue = value;
-                          print(value);
-                        },
+                      const SizedBox(width: 20),
+                      buildCard(
+                        title: "Female",
+                        icon: Icons.female,
                       ),
                     ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: [buildCard(title: 'test', icon: Icons.ac_unit)],
+                const SizedBox(height: 20),
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.teal.shade300,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text("Height"),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            Text(
+                              "${sliderValue.toStringAsFixed(1)}",
+                              style: const TextStyle(
+                                  fontSize: 40, color: Colors.white),
+                            ),
+                            const SizedBox(width: 6),
+                            const Text(
+                              "CM",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                        Slider(
+                          value: sliderValue,
+                          min: 50,
+                          max: 220,
+                          onChanged: (value) {
+                            setState(() {});
+
+                            sliderValue = value;
+                            print(value);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Expanded(flex: 1, child: Container())
-            ],
+                const SizedBox(height: 20),
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.teal.shade300,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const SizedBox(height: 10),
+                              const SizedBox(height: 10),
+                              const Text('Weight'),
+                              Text(
+                                '${weight.toStringAsFixed(1)}',
+                                style: const TextStyle(
+                                    fontSize: 40, color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        weight -= 0.5;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.remove_circle_outline,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        weight += 0.5;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.add_circle_outline,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.teal.shade300,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const SizedBox(height: 10),
+                              const Text('Age'),
+                              Text(
+                                '$age',
+                                style: const TextStyle(
+                                    fontSize: 40, color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        age--;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.remove_circle_outline,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        age++;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.add_circle_outline,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: const Text("Calculate"),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -122,9 +238,9 @@ class _BMIAppState extends State<BMIApp> {
               Icon(
                 icon,
                 color: Colors.white,
-                size: 120,
+                size: 100,
               ),
-              Text(title!)
+              Text(title!),
             ],
           ),
         ),
